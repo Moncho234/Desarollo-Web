8a45721236bdc5c0475f7f08d85c94a412f26f83
Moncho234 <156394606+Moncho234@users.noreply.github.com>
2024-03-04 02:08:23 -0700

    Add files via upload

---
+++ PHP/PHP-SYNTAX.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP SYNTAX</title>
+</head>
+<body>
+
+<?php
+echo "Hola Mundo\n";
+echo "Esto es PHP";
+
+// Este es un comentario de una sola linea
+
+/* Este es un
+comentario
+multi-linea */
+?> 
+
+</body>
+</html>

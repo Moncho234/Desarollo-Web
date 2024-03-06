a9854f212125034dec89797ee940394f99ada7f5
Moncho234 <156394606+Moncho234@users.noreply.github.com>
2024-03-05 17:46:42 -0700

    Add files via upload

---
+++ PHP/PHP-VARIABLES.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP VARIABLES</title>
+</head>
+<body>
+
+<?php
+$txt = "Hola";
+echo $txt;
+echo "<br>";
+$x = 5;
+$y = 7;
+echo $x + $y;
+?> 
+
+</body>
+</html>

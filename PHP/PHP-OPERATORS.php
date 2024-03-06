aceb80bc5c7e273ee1d9240a1b6f752d65825fc8
Moncho234 <156394606+Moncho234@users.noreply.github.com>
2024-03-05 18:18:11 -0700

    Add files via upload

---
+++ PHP/PHP-OPERATORS.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP OPERATORS</title>
+</head>
+<body>
+
+<?php
+echo 10 * 5;
+echo "<br>";
+echo 10 / 2;
+echo "<br>";
+$a = 5;
+$b = 6;
+var_dump($a == $b);
+echo "<br>";
+var_dump($a != $b);
+?> 
+
+</body>
+</html>

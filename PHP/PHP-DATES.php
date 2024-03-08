413c7f5094d02548f6ea285d241ca4c97f968099
Moncho234 <156394606+Moncho234@users.noreply.github.com>
2024-03-07 23:29:21 -0700

    Add files via upload

---
+++ PHP/PHP-DATES.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP DATES</title>
+</head>
+<body>
+
+<?php
+echo date("l");
+echo "<br>";
+echo date("Y.m.d");
+echo "<br>";
+echo date("H:i:s");
+?>
+
+</body>
+</html>
